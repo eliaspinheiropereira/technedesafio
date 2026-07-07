0dd2931e845a4798f50569cce932731030c142ff
Elias P. Pereira <eliaspinheiropereiraa@gmail.com>
2026-07-07 20:22:03 -0300

    iniciando o projeto

---
+++ src/main/java/io/github/eliaspinheiropereira/technedesafio/TechnedesafioApplication.java
@@ -0,0 +1,13 @@
+package io.github.eliaspinheiropereira.technedesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnedesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnedesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/eliaspinheiropereira/technedesafio/TechnedesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eliaspinheiropereira.technedesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnedesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
